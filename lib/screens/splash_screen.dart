a965310abb499bd67702eb8bb8417fd21e531e50
Jhn-Zechariah <paradojhonzechariah@gmail.com>
2026-03-12 12:04:17 +0800

    first commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              isDarkMode
+                  ? 'assets/prism_logo_dark.png'
+                  : 'assets/prism_logo.png',
+              width: 200,
+            ),
+            SizedBox(height: 124),
+          ],
+        ),
+      ),
+    );
+  }
+}
